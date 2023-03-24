26177e1754547023b5fd03a729ffda257c9803b9
ogulcandeniz-inac <https://github.com/ogulcandeniz-inac/github-baglanti.git>
2023-03-24 15:12:00 -0700

    Home page imageView editing Spread the welcome message Animations Screen monitoring view for Welcome and Hello

--- invio-v2/ViewController.swift
+++ invio-v2/ViewController.swift
@@ -9,11 +9,107 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ımageViewRickAndMorty: UIImageView!
+    @IBOutlet weak var ımageViewTitle: UIImageView!
+    @IBOutlet weak var labelTitle: UILabel!
+    var counter:Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped))
+        ımageViewRickAndMorty.addGestureRecognizer(tapGR)
+        ımageViewRickAndMorty.isUserInteractionEnabled = true
+        
+        animation()
+        scaleanimation()
+        
+        
+        
+        let currentCount = UserDefaults.standard.integer(forKey: "launchCount")
+     
+        
+        
+        if currentCount == 0
+        {
+            counter = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(labelWelcome), userInfo: nil, repeats: false)
+            UserDefaults.standard.set(currentCount+1, forKey:"launchCount")
+        }
+        
+        if currentCount >= 1
+        {
+            counter = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(labelhello), userInfo: nil, repeats: false)
+            UserDefaults.standard.set(currentCount-1, forKey:"launchCount")
+        }
+        
+        
+        
+        
+        
     }
 
-
+    
+    
+    
+    
+    
+    @objc func imageTapped(sender: UITapGestureRecognizer)
+    {
+        let next = self.storyboard?.instantiateViewController(withIdentifier: "ViewControllerHomePage") as! ViewControllerHomePage
+            self.navigationController?.pushViewController(next, animated: true)
+    }
+    @objc func labelWelcome( )
+    {
+        labelTitle.text = "Welcome"
+        repetitiveanimation()
+    }
+    @objc func labelhello( )
+    {
+        labelTitle.text = "Hello"
+        repetitiveanimation()
+    }
+    
+    func repetitiveanimation()
+    {
+        UIView.animate(withDuration: 2, delay: 0, options: [.repeat,.autoreverse], animations: { self.labelTitle.transform = CGAffineTransform(translationX: 0, y: 20)}, completion: nil)
+    }
+    func animation()
+    {
+        self.ımageViewTitle.alpha = 1
+        UIView.animate(withDuration: 5,animations:{
+            self.ımageViewTitle.alpha = 0
+        },completion: nil)
+    }
+    func scaleanimation()
+    {
+        UIView.animate(withDuration: 5,animations: {
+            self.ımageViewTitle.transform = CGAffineTransform(scaleX: 2, y: 2)
+        }, completion: nil)
+    }
+    func scaleanimation2()
+    {
+        UIView.animate(withDuration: 5,animations: {
+            self.labelTitle.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }, completion: nil)
+    }
+    func scaleanimation3()
+    {
+        UIView.animate(withDuration: 5,animations: {
+            self.labelTitle.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
---
+++ invio-v2/ViewControllerHomePage.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControllerHomePage.swift
+//  invio-v2
+//
+//  Created by Oğulcan Deniz İnaç on 24.03.2023.
+//
+
+import UIKit
+
+class ViewControllerHomePage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.setHidesBackButton(true, animated:true);
+    }
+    
+
+    
+
+}
