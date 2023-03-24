d134791b9989c3a9880e8b75f2afb7db27cf5d45
ogulcandeniz-inac <https://github.com/ogulcandeniz-inac/github-baglanti.git>
2023-03-24 11:36:54 -0700

    Initial Commit

---
+++ invio-v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  invio-v2
+//
+//  Created by Oğulcan Deniz İnaç on 24.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ invio-v2UITests/invio_v2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  invio_v2UITestsLaunchTests.swift
+//  invio-v2UITests
+//
+//  Created by Oğulcan Deniz İnaç on 24.03.2023.
+//
+
+import XCTest
+
+class invio_v2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
